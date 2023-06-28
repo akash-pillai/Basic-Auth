c20e59dfb9662ee2cfaf60faf905e9aa3d78c13e
akash-pillai <akashjayan96@gmail.com>
2023-06-28 12:27:44 +0530

    [FEATURE] encrypted password

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,19 +1,26 @@
 import { User } from "../entities/user";
 import { addUser, getUserByEmail, getUserById } from "../repositories/userRepository";
 import { generateToken } from "../utils/jwtToken";
+import bcrypt, { hash } from "bcrypt"
 
 
 
 
-export const signUpService=async (user :User)=>{
-
-    const response=await addUser(user);
-    return response;
+export const signUpService = async (user: User) => {
+    try {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        user.password = hashedPassword;
+        const response = await addUser(user);
+        return response;
+    }
+    catch (err) {
+        console.log("something error happened");
+    }
 
 }
 
-export const listUser= async (id :number)=>{
-    const response =await getUserById(id);
+export const listUser = async (id: number) => {
+    const response = await getUserById(id);
     return response;
 }
 
@@ -21,15 +28,14 @@ export const loginUser = async (email: string, password: string) => {
 
     const user = await getUserByEmail(email);
     if (user != null) {
-        if (user.password == password) {
-
+        const isMatch=await bcrypt.compare(password,user.password);
+        if (isMatch) {
             let token: string = generateToken(user);
             return token;
         } else {
-
             return "password doesnt match"
         }
-    }else{
+    } else {
         return "user not found ";
     }
 }
